396459eff58d095ab00abf7094fb61b1161427de
Jack Buehner <jackb@furman.edu>
2026-07-21 16:55:16 -0400

    feat: add timeline storymap

--- src/routes/story/timeline/+page.svelte
+++ src/routes/story/timeline/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+	import { StoryMapEmbed } from '$lib/components';
+</script>
+
 <svelte:head>
 	<title>Interactive Timeline</title>
 </svelte:head>
 
-Under construction
+<StoryMapEmbed
+	storyId="5f9c9e9e40494b0c81041ffed5927214"
+	loadingHeight="calc(100dvh - var(--header-height))"
+/>
