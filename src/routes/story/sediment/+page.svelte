264f21a8da8feb40b5392c1833f30d978831d0d1
Jack Buehner <jackb@furman.edu>
2026-07-21 14:57:46 -0400

    feat: add sediment storymap

--- src/routes/story/sediment/+page.svelte
+++ src/routes/story/sediment/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+	import { StoryMapEmbed } from '$lib/components';
+</script>
+
 <svelte:head>
 	<title>Sediment StoryMap</title>
 </svelte:head>
 
-Under construction
+<StoryMapEmbed
+	storyId="0406f88b70a74be4ac21fd0050fcdd8f"
+	loadingHeight="calc(100dvh - var(--header-height))"
+/>
